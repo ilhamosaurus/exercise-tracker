fde937362b01cb1ab6c2a4db91b55d7fadce3186
Muhammad Ilham Karisma <ilhamsssa@gmail.com>
2023-12-10 11:54:15 +0700

    feat: add postExercise handler

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,5 @@
-const { User } = require("./exercise-db")
+const { ObjectId } = require("mongodb");
+const { User, Exercise } = require("./exercise-db")
 
 const landPageHandler = (req, res) => {
   res.render("index");
@@ -19,10 +20,57 @@ const addUsersHandler = (req, res) => {
 
   newUser.save();
 
+  res.status(200);
   res.json({
     username: newUser.username,
     _id: newUser._id
   });
 }
 
-module.exports = { landPageHandler, getUsersHandler, addUsersHandler }
+const addExerciseHandler = async (req, res) => {
+  const id = req.params._id;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  let date = req.body.date;
+  let unix, newExercise;
+
+  if (!date) {
+    date = new Date().toDateString()
+    unix = new Date().getTime();
+  }
+  else {
+    unix = new Date().getTime();
+    date = new Date().toDateString()
+  }
+
+  const user = await User.findById(id);
+
+  if (!user) {
+    res.status(404).send('Invalid user');
+  }
+
+  try {
+    const newExerciseObject = new Exercise({
+      user: new ObjectId(id),
+      date: date,
+      unix: unix,
+      duration: duration,
+      description: description,
+    });
+
+    newExercise = await newExerciseObject.save();
+  }
+  catch (err) {
+    res.status(400).send(err.message);
+  }
+
+  return res.send({
+    _id: user._id,
+    username: user.username,
+    date: newExercise.date,
+    duration: newExercise.duration,
+    description: newExercise.description,
+  });
+}
+
+module.exports = { landPageHandler, getUsersHandler, addUsersHandler, addExerciseHandler }
--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { landPageHandler, getUsersHandler, addUsersHandler } = require('./handler');
+const { landPageHandler, getUsersHandler, addUsersHandler, addExerciseHandler } = require('./handler');
 const router = express.Router();
 
 router.get('/', landPageHandler);
@@ -7,5 +7,7 @@ router.get('/', landPageHandler);
 router.get('/users', getUsersHandler);
 
 router.post('/users', addUsersHandler);
+
+router.post('/users/:_id/exercises', addExerciseHandler);
 
 module.exports = router;
